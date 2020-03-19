b2f559ae70edc3f18eefdd2a07f11b8a0070e422
Ambroise Lafont <Ambroise.Lafont@data61.csiro.au>
2020-03-19 16:02:26 +1100

    generated files put in an ignored subdir build/

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 
-COGENT_PATH=/home/amblaf/cogent/branches/$1
-$COGENT_PATH/cogent/.stack-work/dist/x86_64-linux-tinfo6/Cabal-2.4.0.1/build/cogent/cogent $2.cogent -g -o $2_$1 --root-dir=$COGENT_PATH -A  --entry-funcs=entrypoints.cfg --fake-header-dir=$COGENT_PATH/cogent/lib/
+COGENT_PATH=/home/laf027/cogent/branches/$1
+mkdir -p build
+$COGENT_PATH/cogent/.stack-work/dist/x86_64-linux/Cabal-2.4.0.1/build/cogent/cogent $2.cogent -g --dist-dir=build -o $2_$1 --root-dir=$COGENT_PATH -A  --entry-funcs=entrypoints.cfg --fake-header-dir=$COGENT_PATH/cogent/lib/
