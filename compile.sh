0fcce642a58cf893314990fb3f739b0a948c10a9
Ambroise Lafont <Ambroise.Lafont@data61.csiro.au>
2020-04-01 17:58:01 +1100

    one build directory per cogent file

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 
 COGENT_PATH=/home/laf027/cogent/branches/$1
-mkdir -p build
-$COGENT_PATH/cogent/.stack-work/dist/x86_64-linux/Cabal-2.4.0.1/build/cogent/cogent $2.cogent -g --dist-dir=build -o $2_$1 --root-dir=$COGENT_PATH -A  --entry-funcs=entrypoints.cfg --fake-header-dir=$COGENT_PATH/cogent/lib/
+BUILD_DIR=build_$2
+mkdir -p $BUILD_DIR
+$COGENT_PATH/cogent/.stack-work/dist/x86_64-linux/Cabal-2.4.0.1/build/cogent/cogent $2.cogent -g --dist-dir=$BUILD_DIR -o $2_$1 --root-dir=$COGENT_PATH -A  --entry-funcs=entrypoints.cfg --fake-header-dir=$COGENT_PATH/cogent/lib/
