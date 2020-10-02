1f86b485ded7795aeed2dbf43ce6d72011e70d02
amblaf <chaster_killer@hotmail.fr>
2020-10-02 10:15:36 +1000

    update

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 
-COGENT_PATH=/home/laf027/cogent/branches/$1
+COGENT_PATH=~/cogent/branches/$1
 BUILD_DIR=build_$2
 mkdir -p $BUILD_DIR
-$COGENT_PATH/cogent/.stack-work/dist/x86_64-linux/Cabal-2.4.0.1/build/cogent/cogent $2.cogent -g --dist-dir=$BUILD_DIR -o $2_$1 --root-dir=$COGENT_PATH -A  --entry-funcs=entrypoints.cfg --fake-header-dir=$COGENT_PATH/cogent/lib/
+$COGENT_PATH/cogent/.stack-work/dist/x86_64-linux-tinfo6/Cabal-2.4.0.1/build/cogent/cogent $2.cogent -g --dist-dir=$BUILD_DIR -o $2_$1 --root-dir=$COGENT_PATH -A  --entry-funcs=entrypoints.cfg --fake-header-dir=$COGENT_PATH/cogent/lib/
