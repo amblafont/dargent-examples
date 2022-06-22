b02f8890d2501d768d51f29d9e88e7b51e9c0eb8
amblaf <chaster_killer@hotmail.fr>
2022-06-22 10:03:27 +0200

    update generated files (seedtrick example)

--- seedtrick/build_random_seed/random_seed.c
+++ seedtrick/build_random_seed/random_seed.c
@@ -7,26 +7,14 @@ typedef struct Seed {
 
 char rand ();
 
-static unsigned long int next = 1;
-
-int rand(void) // RAND_MAX assumed to be 32767
-{
-    next = next * 1103515245 + 12345;
-    return (unsigned int)(next/65536) % 32768;
-}
-
-void srand(unsigned int seed)
-{
-    next = seed;
-}
-
-/*
 SeedValue rand_with_seed(Seed * s) {
+
+   // s->dummy = s->dummy + 1 ?
 
    SeedValue r = 
       // what else?
       { .seed = s, 
         .value = rand() };
+
    return r;
 }
-*/
